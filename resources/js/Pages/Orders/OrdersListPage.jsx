874c25e6e51b69e1475e142f1323045b7a9e1c32
vaishakh3627 <vaishakh3627@gmail.com>
2024-10-16 17:33:17 +0400

    Frontend updates!

---
+++ resources/js/Pages/Orders/OrdersListPage.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import { Card } from "react-bootstrap";
+
+function OrdersListPage() {
+    return (
+        <Card>
+            <Card.Header>Orders</Card.Header>
+        </Card>
+    );
+}
+
+export default OrdersListPage;
